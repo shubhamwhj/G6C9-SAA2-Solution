34972769ad35399e1c6ace22da99baf4f356b2e7
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-08-19 17:50:43 +0530

    Update sketch.js

--- P5 JS SPRITE VELOCITY/sketch.js
+++ P5 JS SPRITE VELOCITY/sketch.js
@@ -3,9 +3,9 @@ function setup() {
   createCanvas(400, 400);
   box = createSprite(40, 40, 40, 40);
   box.shapeColor = color(255);
-  box.position.x = 100;
-  box.position.y = 150;
-  box.velocity.x = 0.5;
+  box.x = 100;
+  box.y = 150;
+  box.velocityX = 5;
   box.rotation = 50;
 }
 
